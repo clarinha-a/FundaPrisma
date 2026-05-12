9feef6030394db80c6ff832eb6bb69ac39b2890d
Ana Clara Campanella Belchior <a.belchior@aluno.ifsp.edu.br>
2026-05-12 07:20:23 -0300

    05-12

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma'
-import { createUser } from '../services/user';
+import { createUser, createUsers } from '../services/user';
 
 export const mainRouter = Router();
 
@@ -19,4 +19,18 @@ mainRouter.post('/user', async (req, res) => {
         res.status(400)
     }
      res.json(user) 
+})
+
+mainRouter.post('/users', async (req, res) => {
+    const result = await createUsers([
+        {name: 'Olive Smith', email:'olive@exemple.com'},
+        {name: 'Adam Carlsen', email:'adama@exemple.com'},
+        {name: 'Levi Ward', email:'leviward@nasa.gov.com'},
+        {name: 'Bee Konigswasser', email:'beekonigswasser@nasa.gov.com'}
+    ])
+    if (result) {
+        res.status(201).json({ ok:true })
+    }else {
+        res.status(400).json({error: 'Error creating users'})
+    }
 })
